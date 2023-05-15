fdb5cd59ee7fd7ef04ac3c0dde1c6011afe92b0f
NathanMorasse <2133133@etu.cchic.ca>
2023-05-15 11:50:21 -0400

    statistics factory

--- APPLICATION/Projet Finale - BD/ProjetFinale/DataAccessLayer/Factory/StatisticsFact.cs
+++ APPLICATION/Projet Finale - BD/ProjetFinale/DataAccessLayer/Factory/StatisticsFact.cs
@@ -1,5 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
 using ProjetFinale.Models;
+using ProjetFinale.DataAccessLayer.Factory.FactoryBase;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace ProjetFinale.DataAccessLayer.Factory
 {
-    internal class StatisticsFact
+    internal class StatisticsFact: FactBase
     {
         private Statistics CreateFromReader(MySqlDataReader reader)
         {
@@ -16,13 +17,46 @@ namespace ProjetFinale.DataAccessLayer.Factory
             int strength = reader.GetInt32("ForcePerso");
             int dexterity = reader.GetInt32("DexteritePerso");
             int constitution = reader.GetInt32("Constitution");
-            int id = reader.GetInt32("IntelligencePerso");
-            int id = reader.GetInt32("SagessePerso");
-            int id = reader.GetInt32("CharismePerso");
-            int id = reader.GetInt32("ArmurePerso");
+            int intelligence = reader.GetInt32("IntelligencePerso");
+            int wisdom = reader.GetInt32("SagessePerso");
+            int charisma = reader.GetInt32("CharismePerso");
+            int armor = reader.GetInt32("ArmurePerso");
 
-            Statistics newSatitistics = new Statistics(id, level, health, name);
+            Statistics newSatitistics = new Statistics(id, strength, dexterity, constitution, intelligence, wisdom, charisma, armor);
             return newSatitistics;
+        }
+
+        public Statistics GetStatisticsByCharacterId(int id)
+        {
+            Statistics statistics = new Statistics();
+            MySqlConnection connection = null;
+            MySqlDataReader reader = null;
+
+            try
+            {
+                connection = new MySqlConnection(this.CnnStr);
+                connection.Open();
+
+                MySqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT * FROM tblStatistics WHERE IdPerso = @Id";
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    statistics = CreateFromReader(reader);
+                }
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                connection.Close();
+            }
+
+            return character;
         }
     }
 }
