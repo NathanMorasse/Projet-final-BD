607d52f6831c73912b8cd8fe37311f647e344d67
NathanMorasse <2133133@etu.cchic.ca>
2023-05-15 22:54:04 -0400

    fin rush ps Mathias est laid as fuck

--- APPLICATION/Projet Finale - BD/ProjetFinale/Views/AbilityPage.xaml.cs
+++ APPLICATION/Projet Finale - BD/ProjetFinale/Views/AbilityPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjetFinale.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,11 @@ namespace ProjetFinale.Views
     /// </summary>
     public partial class AbilityPage : Page
     {
-        public AbilityPage()
+        public AbilityPage(VMPersonnage dataContext)
         {
             InitializeComponent();
+            VMPersonnage vm = dataContext;
+            this.DataContext = vm;
         }
     }
 }
